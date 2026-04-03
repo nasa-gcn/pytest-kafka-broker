14cec428bf8f902e9a7e6a99b0862861fab71ebb
Leo Singer <leo.singer@ligo.org>
2026-04-02 20:10:44 -0400

    Update tests to make use of new context managers

--- tests/test_kafka.py
+++ tests/test_kafka.py
@@ -24,22 +24,14 @@ def test_sync(kafka_broker):
 @pytest.mark.asyncio
 async def test_async(kafka_broker):
     """Demonstrate using the kafka_broker fixture in an async test."""
-    producer = kafka_broker.aio_producer()
-    try:
+    async with kafka_broker.aio_producer() as producer:
         await producer.produce(TOPIC, PAYLOAD)
-    finally:
-        # FIXME: use async context manager; see https://github.com/confluentinc/confluent-kafka-python/pull/2180
-        await producer.close()
-    consumer = kafka_broker.aio_consumer(
-        {"group.id": GROUP_ID, "auto.offset.reset": "earliest"}
-    )
 
-    try:
+    async with kafka_broker.aio_consumer(
+        {"group.id": GROUP_ID, "auto.offset.reset": "earliest"}
+    ) as consumer:
         await consumer.subscribe([TOPIC])
         (message,) = await consumer.consume()
-    finally:
-        # FIXME: use async context manager; see https://github.com/confluentinc/confluent-kafka-python/pull/2180
-        await consumer.close()
     assert message.value() == PAYLOAD
 
     with kafka_broker.admin() as admin:
